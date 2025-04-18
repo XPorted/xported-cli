b797ba098860ececd58c7e51e74a06d265d37330
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-04-19 01:51:11 +0800

    test: add unit tests for findInit function to verify file detection and absence

---
+++ test/utils/FindInit.test.ts
@@ -0,0 +1,42 @@
+import findInit from '../../src/utils/FindInit.js';
+import { jest } from '@jest/globals';
+
+import fs from 'fs';
+import path from 'path';
+
+describe('FindInit', () => {
+	test('should be able to find the .xported init file', () => {
+		const directoryPath = '/tmp/xported-cli-test';
+
+		// Clean up before test
+		if (fs.existsSync(directoryPath))
+			fs.rmdirSync(directoryPath, { recursive: true });
+		fs.mkdirSync(directoryPath, { recursive: true });
+
+		const filePath = path.join(directoryPath, '.xported');
+		fs.writeFileSync(filePath, 'test content');
+		expect(fs.readFileSync(filePath, 'utf8')).toBe('test content'); // To ensure the file was created correctly
+
+		const foundFile = findInit(directoryPath);
+		expect(foundFile).toBe(filePath);
+
+		// Clean up after test
+		fs.unlinkSync(filePath);
+		fs.rmdirSync(directoryPath, { recursive: true });
+	});
+
+	test('should return an empty string if no .xported file is found', () => {
+		const directoryPath = '/tmp/xported-cli-test';
+
+		// Clean up before test
+		if (fs.existsSync(directoryPath))
+			fs.rmdirSync(directoryPath, { recursive: true });		
+		fs.mkdirSync(directoryPath, { recursive: true });
+
+		const foundFile = findInit(directoryPath);
+		expect(foundFile).toBe('');
+		
+
+		fs.rmdirSync(directoryPath, { recursive: true });
+	});
+});
